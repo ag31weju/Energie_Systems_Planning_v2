2ad1781ef92a5fbc8dc413e7659cc4ebd0d137d2
khush.makhija <khush.makhija@stud.tu-darmstadt.de>
2024-11-24 19:12:00 +0100

    added language translation buttons worked a little bit on graphs

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,5 +1,13 @@
 <template>
   <div id="rootdiv" class="grid-row">
+    <div id="language-buttons-container">
+      <button @click="setLanguage('en')" id="language-button-en">
+        <img src="@/assets/en.png" alt="English Flag" />
+      </button>
+      <button @click="setLanguage('de')" id="language-button-de">
+        <img src="@/assets/de.png" alt="German Flag" />
+      </button>
+    </div>
     <div id="outercolumn1" class="grid-column">
       <div id="imagebox" class="grid-column">
         <div id="image"></div>
@@ -28,6 +36,14 @@
             class="h-[30rem]"
           />
         </div>
+        <div id="chart2-box" style="background-color: blue; margin-top: 20px;">
+           <p>Chart 2</p>
+            <Chart
+             type="line"
+            :data="chartset2"
+            :options="chartOptions2"
+            class="h-[30rem]"/>
+        </div>
       </div>
       <div id="checkboxes" class="grid-column">
         <div id="cbrow" class="grid-column">
@@ -52,11 +68,73 @@ import Chart from "primevue/chart";
 
 export default {
   data() {
-    return {
-      sliderVal: 0,
-      sliderVal2: 100,
-    };
-  },
+  return {
+    sliderVal: 0,
+    sliderVal2: 100,
+    // Data for Chart 1
+    chartset: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+      datasets: [
+        {
+          label: 'Graph 1',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1,
+          data: [65, 59, 80, 81, 56, 55],
+        },
+        {
+          label: 'Graph 2',
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+          data: [28, 48, 40, 19, 86, 27],
+        },
+      ],
+    },
+    chartOptions: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: true,
+          position: 'left',
+        },
+      },
+    },
+    // Data for Chart 2
+    chartset2: {
+      labels: ['July', 'August', 'September', 'October', 'November', 'December'],
+      datasets: [
+        {
+          label: 'Graph A',
+          backgroundColor: 'rgba(153, 102, 255, 0.2)',
+          borderColor: 'rgba(153, 102, 255, 1)',
+          borderWidth: 1,
+          data: [45, 67, 75, 50, 40, 90],
+        },
+        {
+          label: 'Graph B',
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1,
+          data: [30, 40, 55, 70, 60, 80],
+        },
+      ],
+    },
+    chartOptions2: {
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: true,
+          position: 'top',
+        },
+      },
+    },
+  };
+},
+
+  
   components: {
     Slider,
     Chart,
