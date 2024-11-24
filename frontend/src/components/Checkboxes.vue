3716be57b734973601df5c327e5d361faa3e3f91
Dennis <dendinescode@gmail.com>
2024-11-24 18:47:11 +0100

    frontend-cleanup:Exported Checkboxes to a seperate vue file

---
+++ frontend/src/components/Checkboxes.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="checkboxes" class="grid-column">
+    <div id="cbrow" class="grid-column">
+      <input type="checkbox" id="ibbbfw" />
+      <input type="checkbox" id="igod8q" />
+      <input type="checkbox" id="ixlfdm" />
+    </div>
+    <div id="cbrow" class="grid-column">
+      <input type="checkbox" id="ikyips" />
+      <input type="checkbox" id="i13dy2" />
+      <input type="checkbox" id="i00u17" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+@import "../assets/main.css";
+</style>
