3ed46679a3e3a6bcb5f98b307a98869a590999db
Dennis <dendinescode@gmail.com>
2024-11-26 21:50:58 +0100

    frontend-feature:added Matrix.vue and a basic linearly scaled matrix

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -21,28 +21,32 @@
     </div>
     <div id="outercolumn2" class="grid-column">
       <div id="matrix" class="grid-column">
-        <Panel header="Matrix"> </Panel>
+        <Panel header="Matrix">
+          <Matrix></Matrix>
+        </Panel>
       </div>
       <div id="charts" class="grid-column">
-    
-        <Panel header="Charts"> <label>Charts</label>
-        <div id="chart1-box" style="background-color: red">
-          <p>Hello</p>
-          <Chart
-            type="bar"
-            :data="chartset"
-            :options="chartOptions"
-            class="h-[30rem]"
-          />
-        </div>
-        <div id="chart2-box" style="background-color: blue; margin-top: 20px;">
-           <p>Chart 2</p>
+        <Panel header="Charts">
+          <label>Charts</label>
+          <div id="chart1-box" style="background-color: red">
+            <p>Hello</p>
             <Chart
-             type="line"
-            :data="chartset2"
-            :options="chartOptions2"
-            class="h-[30rem]"/>
-        </div> </Panel>
+              type="bar"
+              :data="chartset"
+              :options="chartOptions"
+              class="h-[30rem]"
+            />
+          </div>
+          <div id="chart2-box" style="background-color: blue; margin-top: 20px">
+            <p>Chart 2</p>
+            <Chart
+              type="line"
+              :data="chartset2"
+              :options="chartOptions2"
+              class="h-[30rem]"
+            />
+          </div>
+        </Panel>
       </div>
       <Panel header="Checkboxes">
         <Checkboxes></Checkboxes>
@@ -58,82 +62,90 @@ import Sliders from "./components/Sliders.vue";
 import Checkboxes from "./components/Checkboxes.vue";
 import Playfield from "./components/Playfield.vue";
 import Panel from "primevue/panel";
+import Matrix from "./components/Matrix.vue";
 
 export default {
   data() {
-  return {
-    sliderVal: 0,
-    sliderVal2: 100,
-    // Data for Chart 1
-    chartset: {
-      labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-      datasets: [
-        {
-          label: 'Graph 1',
-          backgroundColor: 'rgba(75, 192, 192, 0.2)',
-          borderColor: 'rgba(75, 192, 192, 1)',
-          borderWidth: 1,
-          data: [65, 59, 80, 81, 56, 55],
-        },
-        {
-          label: 'Graph 2',
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          borderColor: 'rgba(255, 99, 132, 1)',
-          borderWidth: 1,
-          data: [28, 48, 40, 19, 86, 27],
-        },
-      ],
-    },
-    chartOptions: {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        legend: {
-          display: true,
-          position: 'left',
+    return {
+      sliderVal: 0,
+      sliderVal2: 100,
+      // Data for Chart 1
+      chartset: {
+        labels: ["January", "February", "March", "April", "May", "June"],
+        datasets: [
+          {
+            label: "Graph 1",
+            backgroundColor: "rgba(75, 192, 192, 0.2)",
+            borderColor: "rgba(75, 192, 192, 1)",
+            borderWidth: 1,
+            data: [65, 59, 80, 81, 56, 55],
+          },
+          {
+            label: "Graph 2",
+            backgroundColor: "rgba(255, 99, 132, 0.2)",
+            borderColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 1,
+            data: [28, 48, 40, 19, 86, 27],
+          },
+        ],
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: true,
+            position: "left",
+          },
         },
       },
-    },
-    // Data for Chart 2
-    chartset2: {
-      labels: ['July', 'August', 'September', 'October', 'November', 'December'],
-      datasets: [
-        {
-          label: 'Graph A',
-          backgroundColor: 'rgba(153, 102, 255, 0.2)',
-          borderColor: 'rgba(153, 102, 255, 1)',
-          borderWidth: 1,
-          data: [45, 67, 75, 50, 40, 90],
-        },
-        {
-          label: 'Graph B',
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderColor: 'rgba(54, 162, 235, 1)',
-          borderWidth: 1,
-          data: [30, 40, 55, 70, 60, 80],
-        },
-      ],
-    },
-    chartOptions2: {
-      responsive: true,
-      maintainAspectRatio: false,
-      plugins: {
-        legend: {
-          display: true,
-          position: 'top',
+      // Data for Chart 2
+      chartset2: {
+        labels: [
+          "July",
+          "August",
+          "September",
+          "October",
+          "November",
+          "December",
+        ],
+        datasets: [
+          {
+            label: "Graph A",
+            backgroundColor: "rgba(153, 102, 255, 0.2)",
+            borderColor: "rgba(153, 102, 255, 1)",
+            borderWidth: 1,
+            data: [45, 67, 75, 50, 40, 90],
+          },
+          {
+            label: "Graph B",
+            backgroundColor: "rgba(54, 162, 235, 0.2)",
+            borderColor: "rgba(54, 162, 235, 1)",
+            borderWidth: 1,
+            data: [30, 40, 55, 70, 60, 80],
+          },
+        ],
+      },
+      chartOptions2: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            display: true,
+            position: "top",
+          },
         },
       },
-    },
-  };
-},
+    };
+  },
 
-  
   components: {
     Checkboxes,
     Sliders,
     Chart,
     Playfield,
     Panel,
+    Matrix,
   },
   methods: {
     async fetchMessage() {
--- frontend/src/components/Matrix.vue
+++ frontend/src/components/Matrix.vue
@@ -1,5 +1,117 @@
-<template></template>
+<template>
+  <div>
+    <canvas id="chart-matrix"></canvas>
+  </div>
+</template>
 
-<script></script>
+<script>
+import { Chart, LinearScale } from "chart.js";
+import { MatrixController, MatrixElement } from "chartjs-chart-matrix";
 
-<style></style>
+// Register matrix plugin with Chart.js globally
+Chart.register(MatrixController, MatrixElement, LinearScale);
+
+export default {
+  data() {
+    return {
+      matrix: null,
+    };
+  },
+  methods: {
+    renderMatrix() {
+      const context = document.getElementById("chart-matrix").getContext("2d");
+      const ex_matrix = [];
+
+      for (let i = 1; i <= 5; i++) {
+        for (let j = 1; j <= 5; j++) {
+          ex_matrix.push({
+            x: i,
+            y: j,
+            v: (i - 1) * 5 + j + 10,
+          });
+        }
+      }
+
+      const ex_data = {
+        datasets: [
+          {
+            label: "My Matrix",
+            data: ex_matrix,
+            backgroundColor(context) {
+              const value = context.dataset.data[context.dataIndex].v;
+              const alpha = (value - 5) / 40;
+              return `rgba(0, 128, 0, ${alpha})`;
+            },
+            borderColor(context) {
+              const value = context.dataset.data[context.dataIndex].v;
+              const alpha = (value - 5) / 40;
+              return `rgba(0, 100, 0, ${alpha})`;
+            },
+            borderWidth: 1,
+            width: ({ chart }) =>
+              ((chart.chartArea || {}).width || 0) /
+                Math.sqrt(ex_matrix.length) -
+              1,
+            height: ({ chart }) =>
+              ((chart.chartArea || {}).height || 0) /
+                Math.sqrt(ex_matrix.length) -
+              1,
+          },
+        ],
+      };
+
+      this.matrix = new Chart(context, {
+        type: "matrix",
+        data: ex_data,
+        options: {
+          plugins: {
+            legend: {
+              display: false,
+            },
+            tooltip: {
+              callbacks: {
+                title() {
+                  return "";
+                },
+                label(context) {
+                  const v = context.dataset.data[context.dataIndex];
+                  return [`x: ${v.x}`, `y: ${v.y}`, `v: ${v.v}`];
+                },
+              },
+            },
+          },
+          scales: {
+            x: {
+              ticks: {
+                stepSize: 1,
+              },
+              grid: {
+                display: false,
+              },
+            },
+            y: {
+              offset: true,
+              ticks: {
+                stepSize: 1,
+              },
+              grid: {
+                display: false,
+              },
+            },
+          },
+        },
+      });
+    },
+  },
+  mounted() {
+    this.renderMatrix();
+  },
+};
+</script>
+
+<style>
+#chart-matrix {
+  max-width: 100%;
+  max-height: 100%;
+}
+</style>
