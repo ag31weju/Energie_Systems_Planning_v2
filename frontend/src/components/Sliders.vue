1587665d5b25286be430e1edad2b8489e40c3d34
Dennis <dendinescode@gmail.com>
2024-11-29 13:28:39 +0100

    frontend: fix slider again

--- frontend/src/components/Sliders.vue
+++ frontend/src/components/Sliders.vue
@@ -1,9 +1,21 @@
 <template>
   <Panel id="sliders" header="Sliders">
-    <Slider v-model="sliderVal" :step="20" class="w-56"></Slider>
-    {{ sliderVal / 20 }}
-    <Slider v-model="sliderVal2" :step="20" class="w-56"></Slider>
-    {{ sliderVal2 / 20 }}
+    <Slider
+      v-model="sliderVal"
+      :min="0"
+      :max="5"
+      :step="step"
+      class="w-56"
+    ></Slider>
+    {{ sliderVal / step }}
+    <Slider
+      v-model="sliderVal2"
+      :min="0"
+      :max="5"
+      :step="step"
+      class="w-56"
+    ></Slider>
+    {{ sliderVal2 / step }}
     <div id="slider-buttons-container">
       <button @click="reset" class="slider-button">Reset</button>
       <button @click="autoSimulate" class="slider-button">Auto Simulate</button>
@@ -19,8 +31,9 @@ import Panel from "primevue/panel";
 export default {
   data() {
     return {
+      step: 1,
       sliderVal: 0,
-      sliderVal2: 100,
+      sliderVal2: 5,
     };
   },
   components: {
