41908ab961488c265da34e2c78637f77d08cec69
Dennis <dendinescode@gmail.com>
2024-11-30 18:43:39 +0100

    frontend: make post method calls for loading scenario and simulation

--- frontend/src/components/Sliders.vue
+++ frontend/src/components/Sliders.vue
@@ -1,25 +1,22 @@
 <template>
   <Panel id="sliders" header="Sliders">
-    <Slider
-      v-model="sliderVal"
-      :min="0"
-      :max="5"
-      :step="step"
-      class="w-56"
-    ></Slider>
-    {{ sliderVal / step }}
-    <Slider
-      v-model="sliderVal2"
-      :min="0"
-      :max="5"
-      :step="step"
-      class="w-56"
-    ></Slider>
-    {{ sliderVal2 / step }}
+    <div v-for="(_, index) in sliderList" :key="index">
+      <Slider
+        v-model="sliderList[index].value"
+        :min="0"
+        :max="5"
+        :step="step"
+        class="w-56"
+      ></Slider>
+      {{ sliderList[index].value / step }}
+    </div>
+
     <div id="slider-buttons-container">
       <button @click="reset" class="slider-button">Reset</button>
-      <button @click="autoSimulate" class="slider-button">Auto Simulate</button>
-      <button @click="simulate" class="slider-button">Simulate</button>
+      <button @click="autoSimulateRequest" class="slider-button">
+        Auto Simulate
+      </button>
+      <button @click="simulateRequest" class="slider-button">Simulate</button>
     </div>
   </Panel>
 </template>
@@ -27,11 +24,14 @@
 <script>
 import Slider from "primevue/slider";
 import Panel from "primevue/panel";
+import axios from "axios";
 
 export default {
   data() {
     return {
+      propData: ["test1", "test2"], //This array tells what the amount and type of nodes are inside the graph
       step: 1,
+      sliderList: [],
       sliderVal: 0,
       sliderVal2: 5,
     };
@@ -40,7 +40,44 @@ export default {
     Slider,
     Panel,
   },
-  methods: {},
+  mounted() {
+    this.sliderList = Array.from(
+      { length: this.propData.length },
+      (_, index) => {
+        return {
+          value: 0,
+          type: this.propData[index],
+        };
+      }
+    );
+  },
+  methods: {
+    reset() {},
+    async simulateRequest() {
+      const url = "someserverendpoint";
+      const data = {
+        sliderList: this.sliderList,
+        reqType: "once",
+      };
+      await this.sendRequest(url, data);
+    },
+    async autoSimulateRequest() {
+      const url = "someserverendpoint";
+      const data = {
+        sliderList: this.sliderList,
+        reqType: "all",
+      };
+      await this.sendRequest(url, data);
+    },
+    async sendRequest(url, data) {
+      try {
+        const response = await axios.post(url, data);
+        console.log("Response:", response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    },
+  },
 };
 </script>
 
