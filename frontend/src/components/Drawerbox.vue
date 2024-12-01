fa7ec4f9d961959023c93197075adf7f133f4532
Paulmp <pplathjr@hotmail.com>
2024-12-01 15:07:50 +0100

    Removed Checkboxes.vue

---
+++ frontend/src/components/Drawerbox.vue
@@ -0,0 +1,29 @@
+<template>
+  <Drawer v-model:visible="visible" header="Drawer" position="top" style="width: 30%;">
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+
+    </p>
+    <Button @click="visible = false">Deactivate Drawer</Button>
+  </Drawer>
+  <Button @click="visible = true">Activate Drawer</Button>
+</template>
+
+<script>
+import { ref } from "vue";
+import Drawer from "primevue/drawer";
+import Button from "primevue/button";
+
+const visible = ref(false);
+
+export default {
+  components: {
+    Drawer,
+    Button,
+  },
+};
+</script>
+
+<style>
+@import "../assets/main.css";
+</style>
