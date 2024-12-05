a314e79c58db463e62783a7dedcc8fea6746fa9c
Dennis <dendinescode@gmail.com>
2024-12-05 19:11:31 +0100

    frontend: partial implementation of loading scenario request

--- frontend/src/components/Playfield.vue
+++ frontend/src/components/Playfield.vue
@@ -1,8 +1,12 @@
 <template>
   <Panel id="playfield">
-    <Button @click="loadRequest" type="submit" class="slider-button">Load Scenario</Button>
-    <img src="../assets/Shlerp.gif" style="min-height: 10%; max-height: 50%; min-width: 10%; max-width: 50%" />
-
+    <Button @click="loadRequest" type="submit" class="slider-button"
+      >Load Scenario</Button
+    >
+    <img
+      :src="imgUrl"
+      style="min-height: 10%; max-height: 50%; min-width: 10%; max-width: 50%"
+    />
   </Panel>
 </template>
 
@@ -12,6 +16,11 @@ import Panel from "primevue/panel";
 import axios from "axios";
 
 export default {
+  data() {
+    return {
+      imgUrl: null,
+    };
+  },
   components: {
     Panel,
     Button,
@@ -20,9 +29,7 @@ export default {
     async loadRequest() {
       try {
         const url = "http://127.0.0.1:8000/api/process-scenario/"; //If frontend and backend have different ports, use the full URL
-        const data = await fetch("scenarios/scene1.json").then((file) =>
-          file.json()
-        );
+        const data = { id: 1 };
         const response = await axios.post(url, data);
         console.log("Response:", response.data);
         // Display a popup with the backend's message
