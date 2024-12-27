d8dd71f62a6f0d17ff2952cb075b48891a40d647
Dennis <dendinescode@gmail.com>
2024-12-27 15:17:06 +0100

    refactored Charts.vue to Composition API

--- frontend/src/components/Charts.vue
+++ frontend/src/components/Charts.vue
@@ -24,6 +24,7 @@
 <script>
 import Chart from "primevue/chart";
 import Panel from "primevue/panel";
+import { ref, watch, onMounted } from "vue";
 
 export default {
   props: {
@@ -65,126 +66,134 @@ export default {
       required: false,
     },
   },
-  watch: {
-    chartsData: {
-      handler(newVal) {
-        if (newVal && Object.keys(newVal).length > 0) {
-          this.updateChart(newVal);
-        } else {
-          console.error("Not good, charts are not receiving data");
-        }
+  setup(props) {
+    const chartset = ref({
+      labels: Array.from({ length: 25 }, (_, i) => i),
+      datasets: [
+        {
+          label: "Storage",
+          backgroundColor: "rgba(153, 102, 255, 0.2)",
+          borderColor: "rgba(153, 102, 255, 1)",
+          borderWidth: 1,
+          data: [],
+        },
+      ],
+    });
+
+    const chartOptions = ref({
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: true,
+          position: "bottom",
+        },
       },
-      deep: true,
-    },
+    });
+
+    const chartset2 = ref({
+      labels: Array.from({ length: 25 }, (_, i) => i),
+      datasets: [
+        {
+          label: "Purchased_Power",
+          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: "rgba(75, 192, 192, 1)",
+          borderWidth: 1,
+          data: [],
+        },
+        {
+          label: "Demand",
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+          data: [],
+        },
+        {
+          label: "PV Production",
+          backgroundColor: "rgba(34, 139, 34, 0.2)",
+          borderColor: "rgba(34, 139, 34, 1)",
+          borderWidth: 1,
+          data: [],
+        },
+        {
+          label: "PV Curtailment",
+          backgroundColor: "rgba(255, 255, 0, 0.2)",
+          borderColor: "rgba(204, 204, 0, 1)",
+          borderWidth: 1,
+          data: [],
+        },
+        {
+          label: "Storage Charge",
+          backgroundColor: "rgba(255, 165, 0, 0.2)",
+          borderColor: "rgba(255, 140, 0, 1)",
+          borderWidth: 1,
+          data: [],
+        },
+        {
+          label: "Storage Discharge",
+          backgroundColor: "rgba(138, 43, 226, 0.2)",
+          borderColor: "rgba(75, 0, 130, 1)",
+          borderWidth: 1,
+          data: [],
+        },
+      ],
+    });
+
+    const chartOptions2 = ref({
+      responsive: true,
+      maintainAspectRatio: false,
+      plugins: {
+        legend: {
+          display: true,
+          position: "bottom",
+        },
+      },
+      scales: {
+        x: {
+          stacked: true,
+        },
+        y: {
+          stacked: true,
+        },
+      },
+    });
+
+    function updateChart(data) {
+      chartset.value.datasets[0].data = data.lineChartData;
+      chartset2.value.datasets[0].data = data.barChartData.purchased_power;
+      chartset2.value.datasets[1].data = data.barChartData.demand;
+      chartset2.value.datasets[2].data = data.barChartData.pv_prodcution;
+      chartset2.value.datasets[3].data = data.barChartData.pv_curtailment;
+      chartset2.value.datasets[4].data = data.barChartData.storage_charge;
+      chartset2.value.datasets[5].data = data.barChartData.storage_discharge;
+    }
+
+    function handleChartsData(newVal) {
+      if (newVal && Object.keys(newVal).length > 0) {
+        updateChart(newVal);
+      } else {
+        console.error("Not good, charts are not receiving data");
+      }
+    }
+
+    watch(
+      () => props.chartsData,
+      (newVal) => handleChartsData(newVal),
+      { deep: true }
+    );
+
+    return {
+      chartset,
+      chartset2,
+      chartOptions,
+      chartOptions2,
+    };
   },
   components: {
     Chart,
     Panel,
   },
-  data() {
-    return {
-      // Data for Chart 1
-      chartset: {
-        labels: Array.from({ length: 25 }, (_, i) => i),
-        datasets: [
-          {
-            label: "Storage",
-            backgroundColor: "rgba(153, 102, 255, 0.2)",
-            borderColor: "rgba(153, 102, 255, 1)",
-            borderWidth: 1,
-            data: [],
-          },
-        ],
-      },
-      chartOptions: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: true,
-            position: "bottom",
-          },
-        },
-      },
-      // Data for Chart 2
-      chartset2: {
-        labels: Array.from({ length: 25 }, (_, i) => i),
-        datasets: [
-          {
-            label: "Purchased_Power",
-            backgroundColor: "rgba(75, 192, 192, 0.2)",
-            borderColor: "rgba(75, 192, 192, 1)",
-            borderWidth: 1,
-            data: [],
-          },
-          {
-            label: "Demand",
-            backgroundColor: "rgba(255, 99, 132, 0.2)",
-            borderColor: "rgba(255, 99, 132, 1)",
-            borderWidth: 1,
-            data: [],
-          },
-          {
-            label: "PV Production",
-            backgroundColor: "rgba(34, 139, 34, 0.2)",
-            borderColor: "rgba(34, 139, 34, 1)",
-            borderWidth: 1,
-            data: [],
-          },
-          {
-            label: "PV Curtailment",
-            backgroundColor: "rgba(255, 255, 0, 0.2)",
-            borderColor: "rgba(204, 204, 0, 1)",
-            borderWidth: 1,
-            data: [],
-          },
-          {
-            label: "Storage Charge",
-            backgroundColor: "rgba(255, 165, 0, 0.2)",
-            borderColor: "rgba(255, 140, 0, 1)",
-            borderWidth: 1,
-            data: [],
-          },
-          {
-            label: "Storage Discharge",
-            backgroundColor: "rgba(138, 43, 226, 0.2)",
-            borderColor: "rgba(75, 0, 130, 1)",
-            borderWidth: 1,
-            data: [],
-          },
-        ],
-      },
-      chartOptions2: {
-        responsive: true,
-        maintainAspectRatio: false,
-        plugins: {
-          legend: {
-            display: true,
-            position: "bottom",
-          },
-        },
-        scales: {
-          x: {
-            stacked: true,
-          },
-          y: {
-            stacked: true,
-          },
-        },
-      },
-    };
-  },
-  methods: {
-    updateChart(data) {
-      this.chartset.datasets[0].data = data.lineChartData;
-      this.chartset2.datasets[0].data = data.barChartData.purchased_power;
-      this.chartset2.datasets[1].data = data.barChartData.demand;
-      this.chartset2.datasets[2].data = data.barChartData.pv_prodcution;
-      this.chartset2.datasets[3].data = data.barChartData.pv_curtailment;
-      this.chartset2.datasets[4].data = data.barChartData.storage_charge;
-      this.chartset2.datasets[5].data = data.barChartData.storage_discharge;
-    },
-  },
+  methods: {},
 };
 </script>
 
