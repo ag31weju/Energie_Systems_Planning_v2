b4df4f85d9d8992e89a6dde2da2cc9446ef2d416
Paulmp <pplathjr@hotmail.com>
2024-12-27 15:51:00 +0100

    "Stash 2/3"

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -8,31 +8,15 @@
   <div id="rootdiv" class="grid-row">
     <div id="outercolumn1" class="grid-column">
       <div id="imagebox" class="grid-column">
-        <Playfield
-          v-bind:load_scenario="load_scenario"
-          v-bind:upload_scenario="upload_scenario"
-          v-bind:toggle_grid="toggle_grid"
-          v-bind:add_consumer="add_consumer"
-          v-bind:add_energy_source="add_energy_source"
-          v-bind:clear_nodes="clear_nodes"
-        ></Playfield>
+        <Playfield></Playfield>
       </div>
       <div id="slider-box">
-        <Sliders
-          v-bind:reset_text="reset_text"
-          v-bind:simulate="simulate"
-          v-bind:auto="auto"
-          @getSimulationData="handleSimulationData"
-        ></Sliders>
+        <Sliders @getSimulationData="handleSimulationData"></Sliders>
       </div>
     </div>
     <div id="outercolumn2" class="grid-column">
       <div id="matrix-box" class="grid-column">
-        <Matrix
-          :matrixData="matrixData"
-          :matrixTheme="matrixTheme"
-          :sliderVals="sliderVals"
-        ></Matrix>
+        <Matrix></Matrix>
       </div>
       <div id="charts-box" class="grid-column">
         <Charts :chartsData="chartsData"></Charts>
