d001e53d5d8457dc356ed3568a719138d80aa423
Dennis <dendinescode@gmail.com>
2025-01-27 16:18:56 +0100

    reorganized my backend code

--- backend/home/views.py
+++ backend/home/views.py
@@ -6,7 +6,7 @@ from .scenario_processing import process_image,parse_json  # Import the external
 from PIL import Image, ImageOps
 import json
 import os, io
-import random, math
+from .response_processing import process_response
 
 
 # Create your views here.
@@ -111,72 +111,7 @@ def save_slider_data(request):
     elif request.method == "GET":
         # Use the gotten Slider data for some computation with the optimizer
 
-        prodCapacities = []
-        autoSimulate = None
-        reset = None
-        
-
-        with open("slider_data/slider_data.json", "r") as sliderData:
-            json_data = json.load(sliderData)
-            prodCapacities = [capacity for capacity in json_data.get("prodCapacities")]
-            autoSimulate = json_data.get("autoSimulate")
-            reset = json_data.get("reset")
-
-        print(prodCapacities)
-        
-        def fill_cell():
-            return {"matrixData": math.floor(random.uniform(0, 100)) ,
-                    "chartsData": {
-                        "lineChartData": [math.floor(random.random() * 100) for _ in range(25)],
-                        "barChartData": {
-                            "purchased_power": [math.floor(random.random() * 100) for _ in range(25)],
-                            "pv_production": [math.floor(random.random() * 100) for _ in range(25)],
-                            "pv_curtailment": [math.floor(random.random() * 100) for _ in range(25)],
-                            "storage_charge": [math.floor(random.random() * 100) for _ in range(25)],
-                            "storage_discharge": [math.floor(random.random() * 100) for _ in range(25)],
-                            "demand": [math.floor(random.random() * -100) for _ in range(25)],
-                            },
-                        },
-                    }
-
-        bestIdx = []
-        def fillWholeStructure(pointer, prodCapacities, rec_depth, bestMatrixVal):
-            if rec_depth == len(prodCapacities):
-                value = fill_cell()
-                if bestMatrixVal[0] > value["matrixData"]: 
-                    bestMatrixVal[0] = value["matrixData"]
-                    bestIdx[:] = prodCapacities[:]
-                return value
-            else:
-                while prodCapacities[rec_depth] <= 5:
-                    pointer[prodCapacities[rec_depth]] = fillWholeStructure(pointer[prodCapacities[rec_depth]], prodCapacities, rec_depth+1, bestMatrixVal)
-                    prodCapacities[rec_depth] += 1
-                prodCapacities[rec_depth] = 0
-            return pointer
-        
-        def initializeNDarray(length):
-            #6 is the amount of steps that can be taken in the slider
-            return [initializeNDarray(length-1) for _ in range(6)] if length != 0 else None
-        
-        #Start filling the data structure on autosimulate, otherwise return a simple object. If reset is true, then don't do anything
-        if not reset: 
-            if autoSimulate:
-                prodCapacities = [0 for x in prodCapacities] #reset prodCapacities to 0
-
-                data = initializeNDarray(len(prodCapacities))
-                #[float("inf")] since it acts as a mutable container for the bestMatrixVal
-                data = {"mainData": fillWholeStructure(data, prodCapacities, 0, [float("inf")]), "bestIdx": bestIdx}
-
-                #just for testing
-                #with open('slider_data/tmp.json', 'w') as file:
-                 #   json.dump(data, file, indent=4)
-            else:
-               data = {"mainData": fill_cell(), "bestIdx": bestIdx}
-        else:
-            data = {"mainData": initializeNDarray(len(prodCapacities)), "bestIdx": bestIdx}
-
-
-        return JsonResponse(data, status=200)
+        return JsonResponse(process_response(), status=200)
     return JsonResponse({"error": "Invalid HTTP method."}, status=405)
 
 @csrf_exempt  
