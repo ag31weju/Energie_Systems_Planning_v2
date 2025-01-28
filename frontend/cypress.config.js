f2c47b4668bdf2169dd62be9edfeceb7d28a88d9
Akshat Gupta <akshat.gupta87651@gmail.com>
2025-01-28 14:03:48 +0100

    linked render url with base url in config.js

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -1,9 +1,13 @@
 import { defineConfig } from 'cypress'
 
+const isLocal = process.env.NODE_ENV === 'development'; // Use `NODE_ENV` to toggle
+const localUrl = 'http://localhost:4173';
+const renderUrl = 'https://energie-systems-planning-v2.onrender.com/';
+
 export default defineConfig({
   e2e: {
     specPattern: 'cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}',
-    baseUrl: 'http://localhost:4173',
+    baseUrl: isLocal ? localUrl : renderUrl,
   },
   component: {
     specPattern: 'src/**/__tests__/*.{cy,spec}.{js,ts,jsx,tsx}',
